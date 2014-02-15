57f4e035cf20783efb11ca5a449c323f9b3267aa
itaylorm <taylor@imaxwell.net>
2014-02-15 07:29:40 -0800

    Initial Solution

---
+++ Named Anonymous Lambda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Named_Anonymous_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
