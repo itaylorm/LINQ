fc48b7d7c5c0aac7dfc5822e3bd5647f0548603e
itaylorm <taylor@imaxwell.net>
2014-03-07 08:31:00 -0800

    Added Sort and Group Example

---
+++ Multiple/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var values = new List<int> {1, 2, 3, 4, 5};
+
+            var maxmin = new {min = values.Min(), max = values.Max()};
+
+
+        }
+    }
+}
---
+++ TupleGroupSort/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TupleGroupSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tuples = new List<MyTuple>
+            {
+                new MyTuple {P1 = "A", P2 = "B"},
+                new MyTuple {P1 = "A", P2 = "C"},
+                new MyTuple {P1 = "D", P2 = "B"}
+            };
+
+            var query = from tuple in tuples
+                orderby tuple.P1
+                group tuple.P2 by tuple.P1
+                into g
+                select new
+                {
+                    Group = g.Key,
+                    Elements = g.OrderByDescending(p2 => p2)
+                };
+
+            foreach (var group in query)
+            {
+                Console.WriteLine("Group: {0}", group.Group);
+                foreach (var value in group.Elements)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("Simpler Syntax");
+
+            var query1 = from tuple in tuples
+                        orderby tuple.P1
+                        group tuple.P2 by tuple.P1;
+
+            foreach (var group in query1)
+            {
+                Console.WriteLine("Group: {0}", group.Key);
+                foreach (var value in group.OrderByDescending(x => x))
+                {
+                    Console.WriteLine("  {0}", value);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public class MyTuple
+        {
+            public string P1 { get; set; }
+            public string P2 { get; set; }
+        }
+
+    }
+}
